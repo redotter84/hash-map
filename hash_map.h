5a977c461ac28568ad4246cdf068ab07f0458cd1
redotter84 <arthemy.redotter@gmail.com>
2020-04-22 06:14:22 +0300

    Update hash_map.h

--- hash_map.h
+++ hash_map.h
@@ -4,276 +4,348 @@
 
 #include <algorithm>
 #include <list>
-#include <vector>
 #include <tuple>
 #include <utility>
-
-const size_t TABLE_SIZE = 3e6;
+#include <vector>
 
 template<class KeyType, class ValueType, class Hash = std::hash<KeyType>>
 class HashMap {
-  private:
-    class Node {
-      public:
-        std::pair<const KeyType, ValueType> item;
+ private:
+  using Node = std::pair<const KeyType, ValueType>;
+  class TableElement;
+  template<class ItemType, class IteratorType>
+  class Iterator;
 
-        Node() {}
+ public:
+  using iterator = Iterator<
+    std::pair<const KeyType, ValueType>,
+    typename std::list<Node>::iterator>;
 
-        Node(std::pair<KeyType, ValueType> item)
-            : item(std::make_pair(item.first, item.second)) {}
-    };
+  using const_iterator = Iterator<
+    const std::pair<const KeyType, ValueType>,
+    typename std::list<Node>::const_iterator>;
 
-    class Vertex {
-      public:
-        bool is_used = false;
-        bool is_erased = false;
-        typename std::list<Node>::iterator iter;
+  HashMap(Hash hasher = Hash());
 
-        Vertex()
-        : is_used(false),
-          is_erased(false) {}
+  template<typename ConstructIterator>
+  explicit HashMap(
+      ConstructIterator begin_iter,
+      ConstructIterator end_iter,
+      Hash hasher = Hash());
 
-        explicit Vertex(typename std::list<Node>::iterator iter)
-        : is_used(true),
-          is_erased(false),
-          iter(iter) {}
-    };
+  explicit HashMap(
+      std::initializer_list<std::pair<KeyType, ValueType>> init,
+      Hash _hasher = Hash());
 
+  HashMap<KeyType, ValueType, Hash>& operator= (
+      const HashMap<KeyType, ValueType, Hash>& other);
 
-    template<class ItemType, class IteratorType>
-    class Iterator {
-      private:
-        IteratorType iter;
+  size_t size() const;
+  bool empty() const;
+  Hash hash_function() const;
 
-      public:
-        Iterator() {}
+  void insert(std::pair<KeyType, ValueType> item);
+  void erase(KeyType key);
+  void clear();
 
-        explicit Iterator(IteratorType iter)
-        : iter(iter) {}
+  ValueType& operator[] (KeyType key);
+  const ValueType& at(KeyType key) const;
 
-        ItemType& operator* () {
-            ItemType* ptr = &(iter->item);
-            return *ptr;
-        }
+  iterator begin();
+  iterator end();
+  const_iterator begin() const;
+  const_iterator end() const;
 
-        ItemType* operator-> () {
-            ItemType* ptr = &(iter->item);
-            return ptr;
-        }
+  iterator find(KeyType key);
+  const_iterator find(KeyType key) const;
 
-        Iterator operator++ (int stuff) {
-            Iterator temp = *this;
-            iter = std::next(iter);
-            return temp;
-        }
+ private:
+  class TableElement {
+   public:
+    bool is_used = false;
+    bool is_erased = false;
+    typename std::list<Node>::iterator iter;
+    TableElement() {}
+    explicit TableElement(typename std::list<Node>::iterator iter)
+        : is_used(true), iter(iter) {}
+  };
 
-        Iterator operator++ () {
-            iter = std::next(iter);
-            return *this;
-        }
-
-        bool operator== (const Iterator& other) {
-            return iter == other.iter;
-        }
-
-        bool operator!= (const Iterator& other) {
-            return iter != other.iter;
-        }
-    };
-
-    Vertex table_[TABLE_SIZE];
-    std::list<Node> nodes_;
-    std::vector<size_t> vertices_;
-    Hash hasher_;
-
-    size_t get_hash(KeyType key) const {
-        const size_t hash = hasher_(key);
-        return hash % TABLE_SIZE;
+  template<class ItemType, class IteratorType>
+  class Iterator {
+   public:
+    Iterator() {}
+    explicit Iterator(IteratorType iter) : iter(iter) {}
+    ItemType& operator* () {
+      ItemType* ptr = &(*iter);
+      return *ptr;
+    }
+    ItemType* operator-> () {
+      ItemType* ptr = &(*iter);
+      return ptr;
+    }
+    Iterator operator++ (int stuff) {
+      Iterator temp = *this;
+      iter = std::next(iter);
+      return temp;
+    }
+    Iterator operator++ () {
+      iter = std::next(iter);
+      return *this;
+    }
+    bool operator== (const Iterator& other) {
+      return iter == other.iter;
+    }
+    bool operator!= (const Iterator& other) {
+      return iter != other.iter;
     }
 
-    std::pair<size_t, size_t> get_position(KeyType key) const {
-        const size_t hash = get_hash(key);
-        size_t pos = hash;
-        while (true) {
-            if (pos >= TABLE_SIZE) {
-                pos = 0;
-            }
-            if ((table_[pos].is_used && !(table_[pos].iter->item.first == key))
-                || table_[pos].is_erased) {
-                ++pos;
-            } else {
-                break;
-            }
-        }
-        return std::make_pair(pos, hash);
-    }
+   private:
+    IteratorType iter;
+  };
 
-  public:
-    using iterator = Iterator<
-        std::pair<const KeyType, ValueType>,
-        typename std::list<Node>::iterator>;
+  const double MAX_LOAD_FACTOR = 0.75;
+  const size_t INIT_SIZE = 3;
 
-    using const_iterator = Iterator<
-        const std::pair<const KeyType, ValueType>,
-        typename std::list<Node>::const_iterator>;
+  size_t GetHash(KeyType key) const;
+  std::pair<size_t, size_t> GetPosition(KeyType key) const;
+  void RebuildTable();
 
-    HashMap(Hash hasher = Hash())
-    : hasher_(hasher) {
-        clear();
-    }
-
-    template<typename ConstructIterator>
-    explicit HashMap(
-        ConstructIterator begin_iter,
-        ConstructIterator end_iter,
-        Hash hasher = Hash()
-    )
-    : hasher_(hasher) {
-        clear();
-        for (ConstructIterator iter = begin_iter; iter != end_iter; iter++) {
-            insert(*iter);
-        }
-    }
-
-    explicit HashMap(
-        std::initializer_list<std::pair<KeyType, ValueType>> init,
-        Hash _hasher = Hash()
-    )
-    : hasher_(_hasher) {
-        clear();
-        for (std::pair<KeyType, ValueType> item : init) {
-            insert(item);
-        }
-    }
-
-    HashMap<KeyType, ValueType, Hash>& operator= (
-        const HashMap<KeyType, ValueType, Hash>& other
-    ) {
-        const HashMap<KeyType, ValueType, Hash> copy = other;
-        clear();
-        hasher_ = copy.hasher_;
-        for (auto iter = copy.begin(); iter != copy.end(); iter++) {
-            insert(*iter);
-        }
-        return *this;
-    }
-
-    size_t size() const {
-        return nodes_.size();
-    }
-
-    bool empty() const {
-        return nodes_.empty();
-    }
-
-    Hash hash_function() const {
-        return hasher_;
-    }
-
-    void insert(std::pair<KeyType, ValueType> item) {
-        size_t pos, hash;
-        std::tie(pos, hash) = get_position(item.first);
-        if (table_[pos].is_used) {
-            return;
-        }
-        nodes_.push_back(Node(item));
-        const auto iter = std::prev(nodes_.end());
-        table_[pos] = Vertex(iter);
-        vertices_.push_back(pos);
-    }
-
-    void erase(KeyType key) {
-        size_t pos, hash;
-        std::tie(pos, hash) = get_position(key);
-        if (!table_[pos].is_used) {
-            return;
-        }
-        auto iter = table_[pos].iter;
-        nodes_.erase(iter);
-        table_[pos] = Vertex();
-        table_[pos].is_erased = true;
-        size_t prv = pos;
-        ++pos;
-        while (true) {
-            if (pos >= TABLE_SIZE) {
-                pos = 0;
-            }
-            if (!table_[pos].is_used) {
-                break;
-            }
-            if (pos == hash) {
-                break;
-            }
-            const size_t other_hash = get_hash(table_[pos].iter->item.first);
-            if (other_hash != hash) {
-                ++pos;
-                continue;
-            }
-            table_[prv] = table_[pos];
-            table_[pos] = Vertex();
-            table_[pos].is_erased = true;
-            prv = pos;
-            ++pos;
-        }
-    }
-
-    ValueType& operator[] (KeyType key) {
-        size_t pos, hash;
-        std::tie(pos, hash) = get_position(key);
-        if (!table_[pos].is_used) {
-            insert(std::make_pair(key, ValueType()));
-        }
-        return table_[pos].iter->item.second;
-    }
-
-    const ValueType& at(KeyType key) const {
-        size_t pos, hash;
-        std::tie(pos, hash) = get_position(key);
-        if (!table_[pos].is_used) {
-            throw std::out_of_range("");
-        }
-        return table_[pos].iter->item.second;
-    }
-
-    void clear() {
-        for (size_t pos : vertices_) {
-            table_[pos] = Vertex();
-        }
-        vertices_.clear();
-        nodes_.clear();
-    }
-
-    iterator begin() {
-        return iterator(nodes_.begin());
-    }
-
-    iterator end() {
-        return iterator(nodes_.end());
-    }
-
-    const_iterator begin() const {
-        return const_iterator(nodes_.begin());
-    }
-
-    const_iterator end() const {
-        return const_iterator(nodes_.end());
-    }
-
-    iterator find(KeyType key) {
-        size_t pos, hash;
-        std::tie(pos, hash) = get_position(key);
-        if (!table_[pos].is_used) {
-            return iterator(nodes_.end());
-        }
-        return iterator(table_[pos].iter);
-    }
-
-    const_iterator find(KeyType key) const {
-        size_t pos, hash;
-        std::tie(pos, hash) = get_position(key);
-        if (!table_[pos].is_used) {
-            return const_iterator(nodes_.end());
-        }
-        return const_iterator(table_[pos].iter);
-    }
+  std::vector<TableElement> table_;
+  std::list<Node> nodes_;
+  std::vector<size_t> elements_indices_;
+  Hash hasher_;
+  size_t table_size_ = 0;
 };
+
+template<class KeyType, class ValueType, class Hash>
+size_t HashMap<KeyType, ValueType, Hash>::GetHash(KeyType key) const {
+  const size_t hash = hasher_(key);
+  return hash % table_size_;
+}
+
+template<class KeyType, class ValueType, class Hash>
+inline std::pair<size_t, size_t> HashMap<KeyType, ValueType, Hash>::GetPosition(
+    KeyType key) const {
+  const size_t hash = GetHash(key);
+  size_t table_index = hash;
+  while (true) {
+    if (table_index >= table_size_) {
+      table_index = 0;
+    }
+    if ((table_[table_index].is_used &&
+      !(table_[table_index].iter->first == key)) ||
+      table_[table_index].is_erased) {
+      ++table_index;
+    } else {
+      break;
+    }
+  }
+  return std::make_pair(table_index, hash);
+}
+
+template<class KeyType, class ValueType, class Hash>
+void HashMap<KeyType, ValueType, Hash>::RebuildTable() {
+  size_t actual_size = elements_indices_.size();
+  double load_factor = (actual_size + 1) * 1.0 / table_size_;
+  if (load_factor < MAX_LOAD_FACTOR) {
+    return;
+  }
+  const HashMap<KeyType, ValueType, Hash> copy = *this;
+  table_size_ = actual_size * ceil(1 / MAX_LOAD_FACTOR);
+  clear();
+  for (auto iter = copy.begin(); iter != copy.end(); iter++) {
+    insert(*iter);
+  }
+}
+
+template<class KeyType, class ValueType, class Hash>
+HashMap<KeyType, ValueType, Hash>::HashMap(Hash hasher)
+    : hasher_(hasher) {
+  clear();
+}
+
+template<class KeyType, class ValueType, class Hash>
+template<typename ConstructIterator>
+inline HashMap<KeyType, ValueType, Hash>::HashMap(
+    ConstructIterator begin_iter,
+    ConstructIterator end_iter,
+    Hash hasher)
+    : hasher_(hasher) {
+  clear();
+  for (ConstructIterator iter = begin_iter; iter != end_iter; iter++) {
+    insert(*iter);
+  }
+}
+
+template<class KeyType, class ValueType, class Hash>
+HashMap<KeyType, ValueType, Hash>::HashMap(
+    std::initializer_list<std::pair<KeyType, ValueType>> init,
+    Hash _hasher)
+    : hasher_(_hasher) {
+  clear();
+  for (std::pair<KeyType, ValueType> item : init) {
+    insert(item);
+  }
+}
+
+template<class KeyType, class ValueType, class Hash>
+HashMap<KeyType, ValueType, Hash>& HashMap<KeyType, ValueType,
+                                           Hash>::operator=(
+    const HashMap<KeyType, ValueType, Hash>& other) {
+  const HashMap<KeyType, ValueType, Hash> copy = other;
+  clear();
+  hasher_ = copy.hasher_;
+  for (auto iter = copy.begin(); iter != copy.end(); iter++) {
+    insert(*iter);
+  }
+  return *this;
+}
+
+template<class KeyType, class ValueType, class Hash>
+size_t HashMap<KeyType, ValueType, Hash>::size() const {
+  size_t ans = nodes_.size();
+  return ans;
+}
+
+template<class KeyType, class ValueType, class Hash>
+bool HashMap<KeyType, ValueType, Hash>::empty() const {
+  return nodes_.empty();
+}
+
+template<class KeyType, class ValueType, class Hash>
+Hash HashMap<KeyType, ValueType, Hash>::hash_function() const {
+  return hasher_;
+}
+
+template<class KeyType, class ValueType, class Hash>
+void HashMap<KeyType, ValueType, Hash>::insert(
+    std::pair<KeyType, ValueType> item) {
+  RebuildTable();
+  size_t table_index, hash;
+  std::tie(table_index, hash) = GetPosition(item.first);
+  if (table_[table_index].is_used) {
+    return;
+  }
+  nodes_.push_back(Node(item));
+  const auto iter = std::prev(nodes_.end());
+  table_[table_index] = TableElement(iter);
+  elements_indices_.push_back(table_index);
+}
+
+template<class KeyType, class ValueType, class Hash>
+void HashMap<KeyType, ValueType, Hash>::erase(KeyType key) {
+  size_t table_index, hash;
+  std::tie(table_index, hash) = GetPosition(key);
+  if (!table_[table_index].is_used) {
+    return;
+  }
+  auto iter = table_[table_index].iter;
+  nodes_.erase(iter);
+  table_[table_index] = TableElement();
+  table_[table_index].is_erased = true;
+  size_t prv = table_index;
+  ++table_index;
+  while (true) {
+    if (table_index >= table_size_) {
+      table_index = 0;
+    }
+    if (!table_[table_index].is_used) {
+      break;
+    }
+    if (table_index == hash) {
+      break;
+    }
+    const size_t other_hash = GetHash(table_[table_index].iter->first);
+    if (other_hash != hash) {
+      ++table_index;
+      continue;
+    }
+    table_[prv] = table_[table_index];
+    table_[table_index] = TableElement();
+    table_[table_index].is_erased = true;
+    prv = table_index;
+    ++table_index;
+  }
+}
+
+template<class KeyType, class ValueType, class Hash>
+ValueType& HashMap<KeyType, ValueType, Hash>::operator[](KeyType key) {
+  size_t table_index, hash;
+  std::tie(table_index, hash) = GetPosition(key);
+  if (!table_[table_index].is_used) {
+    insert(std::make_pair(key, ValueType()));
+  }
+  std::tie(table_index, hash) = GetPosition(key);
+  return table_[table_index].iter->second;
+}
+
+template<class KeyType, class ValueType, class Hash>
+const ValueType& HashMap<KeyType, ValueType, Hash>::at(KeyType key) const {
+  size_t table_index, hash;
+  std::tie(table_index, hash) = GetPosition(key);
+  if (!table_[table_index].is_used) {
+    throw std::out_of_range("");
+  }
+  return table_[table_index].iter->second;
+}
+
+template<class KeyType, class ValueType, class Hash>
+void HashMap<KeyType, ValueType, Hash>::clear() {
+  for (size_t table_index : elements_indices_) {
+    table_[table_index] = TableElement();
+  }
+  table_size_ = std::max(table_size_, INIT_SIZE);
+  table_.resize(table_size_);
+  elements_indices_.clear();
+  nodes_.clear();
+}
+
+template<class KeyType, class ValueType, class Hash>
+typename HashMap<KeyType, ValueType,
+                 Hash>::iterator HashMap<KeyType, ValueType, Hash>::begin() {
+  return iterator(nodes_.begin());
+}
+
+template<class KeyType, class ValueType, class Hash>
+typename HashMap<KeyType, ValueType,
+                 Hash>::iterator HashMap<KeyType, ValueType, Hash>::end() {
+  return iterator(nodes_.end());
+}
+
+template<class KeyType, class ValueType, class Hash>
+typename HashMap<KeyType, ValueType,
+                 Hash>::const_iterator HashMap<KeyType, ValueType,
+                                               Hash>::begin() const {
+  return const_iterator(nodes_.begin());
+}
+
+template<class KeyType, class ValueType, class Hash>
+typename HashMap<KeyType, ValueType,
+                 Hash>::const_iterator HashMap<KeyType, ValueType,
+                                               Hash>::end() const {
+  return const_iterator(nodes_.end());
+}
+
+template<class KeyType, class ValueType, class Hash>
+typename HashMap<KeyType, ValueType,
+                 Hash>::iterator HashMap<KeyType, ValueType,
+                                         Hash>::find(KeyType key) {
+  size_t table_index, hash;
+  std::tie(table_index, hash) = GetPosition(key);
+  if (!table_[table_index].is_used) {
+    return iterator(nodes_.end());
+  }
+  return iterator(table_[table_index].iter);
+}
+
+template<class KeyType, class ValueType, class Hash>
+typename HashMap<KeyType, ValueType,
+                 Hash>::const_iterator HashMap<KeyType, ValueType,
+                                               Hash>::find(KeyType key) const {
+  size_t table_index, hash;
+  std::tie(table_index, hash) = GetPosition(key);
+  if (!table_[table_index].is_used) {
+    return const_iterator(nodes_.end());
+  }
+  return const_iterator(table_[table_index].iter);
+}
